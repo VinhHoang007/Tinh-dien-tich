cf9cc1399e148888a058309525c87b215fa2a35e
unknown <vinhhoangngoc22@gmail.com>
2021-01-31 21:09:31 +0700

    Tinh dien tich

---
+++ myscript.js
@@ -0,0 +1,8 @@
+function HCN(){
+    let inputWidth = prompt("Nhap chieu rong");
+    let inputHeight = prompt("Nhap chieu dai");
+    let width = parseInt(inputWidth);
+    let height = parseInt(inputHeight);
+    let area = width * height;
+    document.write(area);
+}
